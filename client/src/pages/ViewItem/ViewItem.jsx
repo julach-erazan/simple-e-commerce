c50d90e6441a7dacbc45f462ff1c08cffd129492
Julach Earzan <114212741+julach-erazan@users.noreply.github.com>
2024-12-02 19:28:55 +0530

    initialized the project and created components
    create login page
    create registation page
    create home page
    create navigation bar
    create product card
    create product list

---
+++ client/src/pages/ViewItem/ViewItem.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import ViewItemForm from '../../components/ViewItemForm/ViewItemForm'
+
+const ViewItem = () => {
+  return (
+    <div>
+      <div>
+        Image, Name, Description
+      </div>
+      <ViewItemForm/>
+    </div>
+  )
+}
+
+export default ViewItem
