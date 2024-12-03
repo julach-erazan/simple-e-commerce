7ba009ebe9a8dea38dd38654037df375174264e4
Julach Earzan <114212741+julach-erazan@users.noreply.github.com>
2024-12-03 09:38:29 +0530

    developed login

--- client/src/components/LoginForm/LoginForm.js
+++ client/src/components/LoginForm/LoginForm.js
@@ -1,10 +1,86 @@
 
 import React from 'react'
+import { useEffect, useState } from "react";
 
 const LoginForm = () => {
+
+  const [token, setToken] = useState();
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    const formData = new FormData(event.target);
+
+    // Logging form data
+    const email = formData.get("email");
+    const password = formData.get("password");
+
+    fetch("https://fakestoreapi.com/auth/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        username: email, //"mor_2314"
+        password: password, //"83r5^_"
+      }),
+    })
+      .then((res) => res.json())
+      .then((json) => {
+        if (json.token) {
+          sessionStorage.setItem("token", json.token);
+          window.location.reload();
+        } else {
+          alert("Invalid Email or Password !");
+        }
+      })
+      .catch((err) => alert("Invalid Email or Password !"));
+  };
+
+  useEffect(() => {
+    setToken(sessionStorage.getItem("token"));
+  }, []);
+
+  
+
   return (
-    <div>
-      
+    <div className='loginForm'>
+      <form
+          onSubmit={handleSubmit}
+          className="w-[90%] md:w-[400px] flex flex-col"
+        >
+          <h1 className="text-[30px] text-[#2F3C7E] text-center font-bold mb-[20px]">
+            Login in to your account
+          </h1>
+          <h2 className="text-[15px] text-[#E4552D] text-center font-semibold mb-[20px]">
+            <u>
+              <a href="/register?data=1">No account? Create one here</a>
+            </u>
+          </h2>
+
+          <label htmlFor="email" className="text-left">
+            Email
+          </label>
+          <input type="text" id="email" name="email" />
+
+          <label htmlFor="password" className="text-left">
+            Password
+          </label>
+          <input type="password" id="password" name="password" />
+
+          <h2 className="text-[15px] text-[#E4552D] text-center font-semibold">
+            <u>
+              <a href="#">Forgot your password?</a>
+            </u>
+          </h2>
+
+          <button
+            className="h-[40px] text-[#fff] font-semibold bg-[#2F3C7E] hover:bg-[#E4552D] mt-[50px]"
+            type="submit"
+          >
+            Sign in
+          </button>
+        </form>
     </div>
   )
 }
--- client/src/pages/User/Login/Login.js
+++ client/src/pages/User/Login/Login.js
@@ -1,11 +1,36 @@
-import React from 'react'
+
+import Footer from "../../../components/Footer/Footer";
+import LoginForm from "../../../components/LoginForm/LoginForm";
 
 const Login = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  
+  const token = ""
 
-export default Login
+  const handleLogout = () => {
+    sessionStorage.removeItem("token");
+    window.location.reload();
+  };
+  
+  return (
+    <div className="w-full min-w-[350px] flex flex-col justify-center items-center pt-[150px]">
+      {token ? (
+        <div className="w-[90%] md:w-[400px] h-[300px] flex flex-col">
+          <h1 className="text-[30px] text-[#2F3C7E] text-center font-bold mb-[20px]">
+            Loggin Successfully
+          </h1>
+          <button
+            className="h-[40px] text-[#fff] font-semibold bg-[#2F3C7E] hover:bg-[#E4552D] mt-[100px]"
+            onClick={handleLogout}
+          >
+            Logout
+          </button>
+        </div>
+      ) : (
+        <LoginForm/>
+      )}
+      <Footer />
+    </div>
+  );
+};
+
+export default Login;
