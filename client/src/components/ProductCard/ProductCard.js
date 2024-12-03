8a366f4e264e8c3cbb1e4a64089d7e5b79f968db
Julach Earzan <114212741+julach-erazan@users.noreply.github.com>
2024-12-03 11:57:35 +0530

    Developed ProductCard

--- client/src/components/ProductCard/ProductCard.js
+++ client/src/components/ProductCard/ProductCard.js
@@ -1,11 +1,28 @@
-import React from 'react'
+import React from "react";
 
-const ProductCard = () => {
+const ProductCard = (props) => {
+
   return (
     <div>
-      
+      <a href={`/ViewItem?id=${props.data.id}`}>
+        <div className="w-[300px] h-[400px] flex flex-col justify-center items-center border-[5px] border-solid border-[#cacafa] hover:border-[5px] hover:border-solid hover:border-[#2F3C7E]">
+          <img
+            src={props.data.image}
+            alt="produccts"
+            className="w-[280px] h-[280px]"
+          />
+          <div className="w-full h-[90px] flex justify-between items-center p-[10px]">
+            <h1 className="w-[70%] h-full text-[#2F3C7E] flex justify-center items-center">
+              {props.data.title}
+            </h1>
+            <h1 className="w-[20%] h-full text-[#E4552D] font-bold flex justify-center items-center">
+              {props.data.price}
+            </h1>
+          </div>
+        </div>
+      </a>
     </div>
-  )
-}
+  );
+};
 
-export default ProductCard
+export default ProductCard;
