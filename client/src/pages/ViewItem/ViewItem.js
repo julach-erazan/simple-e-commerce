83f900a680866bc0b001ab0a69a48afe5fa0cd61
Julach Earzan <114212741+julach-erazan@users.noreply.github.com>
2024-12-03 13:04:23 +0530

    Update ViewItem.js and ViewItemCard component

--- client/src/pages/ViewItem/ViewItem.js
+++ client/src/pages/ViewItem/ViewItem.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import Footer from "../../components/Footer/Footer";
 import ViewItemCard from "../../components/ViewItemCard/ViewItemCard";
 
@@ -6,16 +6,6 @@ const ViewItem = () => {
 
   return (
     <div className="w-full min-w-[350px] pt-[100px]">
-      {/* <ul>
-        {products.map((product, index) => (
-          <li
-            key={index}
-            className="w-full flex flex-col lg:flex-row justify-center items-center p-[25px]"
-          >
-            
-          </li>
-        ))}
-      </ul> */}
       <ViewItemCard/>
       <Footer/>
     </div>
