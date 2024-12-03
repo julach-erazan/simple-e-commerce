a6c7d5cf005cc7b3f7d61b740fbca974515993a4
Julach Earzan <114212741+julach-erazan@users.noreply.github.com>
2024-12-03 13:03:51 +0530

    Update ProductCard.js

--- client/src/components/ProductCard/ProductCard.js
+++ client/src/components/ProductCard/ProductCard.js
@@ -7,16 +7,18 @@ const ProductCard = (props) => {
       <a href={`/ViewItem?id=${props.data.id}`}>
         <div className="w-[300px] h-[400px] flex flex-col justify-center items-center border-[5px] border-solid border-[#cacafa] hover:border-[5px] hover:border-solid hover:border-[#2F3C7E]">
           <img
-            src={props.data.image}
+            src={props.data.imageURL}
             alt="produccts"
-            className="w-[280px] h-[280px]"
+            className="w-[280px] h-[280px] object-cover"
           />
           <div className="w-full h-[90px] flex justify-between items-center p-[10px]">
-            <h1 className="w-[70%] h-full text-[#2F3C7E] flex justify-center items-center">
-              {props.data.title}
-            </h1>
+            <div className="w-[70%] h-full text-[#2F3C7E] flex flex-col justify-center items-center">
+              <h1 className="text-[16px] font-bold text-center">{props.data.name}</h1>
+              <p className="text-[13px] text-center text-[#777]">{props.data.description}</p>
+            </div>
+            <p></p>
             <h1 className="w-[20%] h-full text-[#E4552D] font-bold flex justify-center items-center">
-              {props.data.price}
+              {props.data.price} $
             </h1>
           </div>
         </div>
