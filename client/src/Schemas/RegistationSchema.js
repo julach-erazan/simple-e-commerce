948a3548c54db398da7e0dab7f62fb644d94a0fe
Julach Earzan <114212741+julach-erazan@users.noreply.github.com>
2024-12-04 11:30:09 +0530

    validate register

---
+++ client/src/Schemas/RegistationSchema.js
@@ -0,0 +1,20 @@
+import * as yup from "yup";
+
+const passwordRules = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{8,15}$/;
+//"Must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters
+
+export const formSchema = yup.object().shape({
+    userName: yup.string().required("Required"),
+    firstName: yup.string().required("Required"),
+    lastName: yup.string().required("Required"),
+    email: yup.string().email("Please enter a valid email").required("Required"),
+    password: yup
+        .string()
+        .min(8)
+        .matches(passwordRules, "Please create a strong password")
+        .required("Required"),
+    confirmPassword: yup
+        .string()
+        .oneOf([yup.ref("password"), null], "Please must match")
+        .required("Required"),
+});
